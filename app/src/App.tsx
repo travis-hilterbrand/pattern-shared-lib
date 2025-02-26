b60356b00ffaed59f538fddb12b1dc59339ecda8
Travis Hilterbrand <travis.hilterbrand@zingly.ai>
2025-02-26 14:54:07 -0700

    test

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@me/shared/shared";
+
 function App() {
   return (
     <div>
       <div>App</div>
       <hr />
+      <Button label={"My Button"} />
     </div>
   );
 }
